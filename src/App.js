0919492d947ea82ef74a57c5ccaf2dbc09f6522c
Jammel Johnson <jammeljohnson@gmail.com>
2025-06-21 18:16:55 -0500

    Update prop-types dependency to use caret versioning; add Button component with prop validation to enhance UI functionality in App component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Button from "./Button";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Button success primary>
+          Click Here
+        </Button>
+      </div>
+      <div>
+        <Button>Click Here</Button>
+      </div>
+      <div>
+        <Button>Click Here</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
