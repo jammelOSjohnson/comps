8158063a3451aead99b16de1ad67c7172c68cef4
Jammel Johnson <jammeljohnson@gmail.com>
2025-06-21 19:52:03 -0500

    Update Button components in App.js to include icons for enhanced visual appeal; modify button styles for better user interaction.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from "react";
 import Button from "./Button";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
 
 function App() {
   return (
     <div>
       <div>
-        <Button success rounded primary>
+        <Button success rounded outline>
+          <GoBell />
           Click Here
         </Button>
       </div>
       <div>
         <Button danger outline>
+          <GoCloudDownload />
           Click Here
         </Button>
       </div>
       <div>
-        <Button warning>Click Here</Button>
+        <Button warning>
+          <GoDatabase />
+          Click Here
+        </Button>
       </div>
       <div>
         <Button secondary outline>
+          Click Here
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
           Click Here
         </Button>
       </div>
