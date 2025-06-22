55d6fc7542fe841edf886f6a86712302bf274fef
Jammel Johnson <jammeljohnson@gmail.com>
2025-06-21 19:39:08 -0500

    Add Tailwind CSS integration; import index.css in main entry file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
 // 1. Get a reference to the div with the id of root
 const el = document.getElementById("root");
